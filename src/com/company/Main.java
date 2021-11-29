37bd94c659eefec2d4a2af34835fbafbe43d4505
YANATIMAKANA <YAYAKUZE@GMAIL.COM>
2021-11-29 15:59:06 +0200

    STARTING POINT

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("***************************TimeTable*********************");
+        System.out.println("********ACCOUNTING*************-15:30********");
+        System.out.println("********MATHS********-14:30********");
+        System.out.println("***********CAT********-13:30");
+
+    }
+}
+
